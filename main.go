1d9758a5708b35aff559940c004ce537bbea6a71
liumuxiao <65821229+liumuxiao@users.noreply.github.com>
2023-11-30 23:54:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -287,7 +287,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "灵柒",  "沐沐"),
+		NickName:       append([]string{*adana}, "灵柒",  "沐沐", "沐"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
